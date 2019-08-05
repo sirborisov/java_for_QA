1cb572b461bc32543732f99e7b76e3de587b3d2d
Vladimir Borisov <v.borisov@litebox.ru>
2019-08-05 21:55:19 +0400

    Удалено вычисление площади квадрата

--- sandbox/src/main/java/ru/qa/borisov/sandbox/FirstProgram.java
+++ sandbox/src/main/java/ru/qa/borisov/sandbox/FirstProgram.java
@@ -2,11 +2,12 @@ package ru.qa.borisov.sandbox;
 
 public class FirstProgram {
   public static void main(String[] args) {
-    String one = "world";
-    System.out.println("Hello, " +  one + "!");
-
-    double l = 5.25; ///длина стороны
-    double s = l*l; ///площадь квадрата
-    System.out.println("Площадь квадарата со стороной " + l + " равна " + s);
+    hello();
   }
+
+  public static void hello() {
+    String one = "world";
+    System.out.println("Hello, " + one + "!");
+  }
+
 }
