2a6bb1701156a9a9651f011b09343f3a99667bb7
Vladimir Borisov <v.borisov@litebox.ru>
2019-08-09 00:59:03 +0400

    вычисление расстояния между двумя точками на плоскости (функция заменена методом)

--- sandbox/src/main/java/ru/qa/borisov/homework2/Homework2.java
+++ sandbox/src/main/java/ru/qa/borisov/homework2/Homework2.java
@@ -6,13 +6,8 @@ public class Homework2 {
 
   public static void main(String[] args) {
 
-    Point p1 = new Point(1, 2);
-    Point p2 = new Point(3, 4);
-    System.out.println("Расстояние между точками с координатами " + p1.x + "," + p1.z + " и " + p2.x + "," + p2.z + " равняется " + distance(p1, p2));
+    Point p = new Point(1, 2, 3, 4);
+    System.out.println("Расстояние между точками с координатами " + p.x1 + "," + p.z1 + " и " + p.x2 + "," + p.z2 + " равняется " + p.distance());
 
-  }
-
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt(((p2.x - p1.x) * (p2.x - p1.x)) + ((p2.z - p1.z) * (p2.z - p1.z)));
   }
 }
--- sandbox/src/main/java/ru/qa/borisov/homework2/Point.java
+++ sandbox/src/main/java/ru/qa/borisov/homework2/Point.java
@@ -2,12 +2,20 @@ package ru.qa.borisov.homework2;
 
 public class Point {
 
-  public double x;
-  public double z;
+  public double x1;
+  public double z1;
+  public double x2;
+  public double z2;
 
-  public Point (double x, double z) {
-    this.x = x;
-    this.z = z;
+  public Point (double x1, double z1, double x2, double z2) {
+    this.x1 = x1;
+    this.z1 = z1;
+    this.x2 = x2;
+    this.z2 = z2;
+  }
+
+  public double distance () {
+    return Math.sqrt(((this.x2 - this.x1) * (this.x2 - this.x1)) + ((this.z2 - this.z1) * (this.z2 - this.z1)));
   }
 
 }
