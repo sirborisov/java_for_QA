003421841a31800b805f5ef13a9c94b2d654133f
Vladimir Borisov <v.borisov@litebox.ru>
2019-08-28 15:09:01 +0400

    Добавлен тест создания нового контакта

---
+++ addressbook-web-tests/src/test/java/ru/qa/borisov/addressbook/ContactData.java
@@ -0,0 +1,43 @@
+package ru.qa.borisov.addressbook;
+
+public class ContactData {
+  private final String firstname;
+  private final String lastname;
+  private final String nickname;
+  private final String address;
+  private final String mobile;
+  private final String email;
+
+  public ContactData(String firstname, String lastname, String nickname, String address, String mobile, String email) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.nickname = nickname;
+    this.address = address;
+    this.mobile = mobile;
+    this.email = email;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getMobile() {
+    return mobile;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
