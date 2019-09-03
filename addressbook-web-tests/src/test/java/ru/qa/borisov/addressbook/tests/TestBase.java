6e2d60212b8cc5aadd433051c5f9c1e7bf5a4808
Vladimir Borisov <v.borisov@litebox.ru>
2019-09-03 13:35:53 +0400

    Добавлена возможность запуска тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/qa/borisov/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/qa/borisov/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package ru.qa.borisov.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.qa.borisov.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
