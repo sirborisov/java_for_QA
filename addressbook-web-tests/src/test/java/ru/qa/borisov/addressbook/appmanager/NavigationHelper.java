c031b131446cf21673834f8c389432fbaefaa8d1
Vladimir Borisov <v.borisov@litebox.ru>
2019-09-03 11:07:08 +0400

    Тест с условиями

--- addressbook-web-tests/src/test/java/ru/qa/borisov/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/qa/borisov/addressbook/appmanager/NavigationHelper.java
@@ -2,7 +2,6 @@ package ru.qa.borisov.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class NavigationHelper extends HelperBase {
 
