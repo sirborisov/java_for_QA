71a3523101a8d60ab969951a5afef824e9c6e41c
Vladimir Borisov <v.borisov@litebox.ru>
2019-09-03 11:40:57 +0400

    Свёртка вложенных проверок

--- sandbox/src/main/java/ru/qa/borisov/sandbox/Equation.java
+++ sandbox/src/main/java/ru/qa/borisov/sandbox/Equation.java
@@ -6,7 +6,7 @@ public class Equation {
   private double b;
   private double c;
 
-  private  int n;
+  private int n;
 
   public Equation(double a, double b, double c) {
 
@@ -14,18 +14,28 @@ public class Equation {
     this.b = b;
     this.c = c;
 
-    double d = b*b - 4*a*c;
+    double d = b * b - 4 * a * c;
 
-    if(d>0) {
-      n = 2;
-    } else {
-      if (d==0) {
+    if (a != 0) {
+      if (d > 0) {
+        n = 2;
+      } else if (d == 0) {
         n = 1;
       } else {
         n = 0;
       }
+
+    } else if (b != 0) {
+      n = 1;
+
+    } else if (c != 0) {
+      n = 0;
+
+    } else {
+      n = -1;
     }
   }
+
   public int rootNumber() {
     return n;
   }
