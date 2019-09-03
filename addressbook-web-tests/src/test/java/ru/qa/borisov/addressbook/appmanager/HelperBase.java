f6ff3fd8fc1a7e9eadfeb3d87bb742cae172f9e1
Vladimir Borisov <v.borisov@litebox.ru>
2019-09-03 14:31:22 +0400

    Заполнение полей дефолтными значениями

--- addressbook-web-tests/src/test/java/ru/qa/borisov/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/qa/borisov/addressbook/appmanager/HelperBase.java
@@ -12,8 +12,10 @@ public class HelperBase {
 
   public void type(By locator, String text) {
     click(locator);
-    wd.findElement(locator).clear();
-    wd.findElement(locator).sendKeys(text);
+    if (text != null) {
+      wd.findElement(locator).clear();
+      wd.findElement(locator).sendKeys(text);
+    }
   }
 
   public void expect (By locator){
