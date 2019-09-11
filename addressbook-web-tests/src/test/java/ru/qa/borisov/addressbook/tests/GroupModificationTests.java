dff09ee080bc0e8e2b609d5bcc2885750beee7e3
Vladimir Borisov <v.borisov@litebox.ru>
2019-09-11 20:40:57 +0400

    Выбор элемента списка

--- addressbook-web-tests/src/test/java/ru/qa/borisov/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/qa/borisov/addressbook/tests/GroupModificationTests.java
@@ -14,7 +14,7 @@ public class GroupModificationTests extends TestBase {
     if (!app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("Test1", "Test2", "Test3"));
     }
-    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().selectGroup(before-1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("Test4", "test2", "Test3"));
     app.getGroupHelper().submitGroupModification();
