513bb8ba66eb402c708c0e535370c9d989db8be0
Vladimir Borisov <v.borisov@litebox.ru>
2019-09-16 11:56:43 +0400

    Анонимные функции

--- addressbook-web-tests/src/test/java/ru/qa/borisov/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/qa/borisov/addressbook/tests/GroupCreationTests.java
@@ -3,7 +3,6 @@ package ru.qa.borisov.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.qa.borisov.addressbook.model.GroupData;
-
 import java.util.HashSet;
 import java.util.List;
 
@@ -18,14 +17,7 @@ public class GroupCreationTests extends TestBase {
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-
-    int max = 0;
-    for (GroupData g : after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    group.setId(max);
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
