df1b8082410c480946ca7626d7ea4418a96bc4a7
Vladimir Borisov <v.borisov@litebox.ru>
2019-09-16 13:57:04 +0400

    Сортировка списков в тестах Создания и Модификации групп

--- addressbook-web-tests/src/test/java/ru/qa/borisov/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/qa/borisov/addressbook/tests/GroupCreationTests.java
@@ -3,6 +3,8 @@ package ru.qa.borisov.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.qa.borisov.addressbook.model.GroupData;
+
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -19,7 +21,10 @@ public class GroupCreationTests extends TestBase {
 
     group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
-    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
   }
 
 }
