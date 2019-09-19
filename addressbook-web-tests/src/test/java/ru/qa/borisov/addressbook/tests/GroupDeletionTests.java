a00680b9902b3ba52bbdf9fab93033053eb645f6
Vladimir Borisov <v.borisov@litebox.ru>
2019-09-19 18:08:46 +0400

    Реструктуризация тестов групп

--- addressbook-web-tests/src/test/java/ru/qa/borisov/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/qa/borisov/addressbook/tests/GroupDeletionTests.java
@@ -1,6 +1,7 @@
 package ru.qa.borisov.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.qa.borisov.addressbook.model.GroupData;
 
@@ -8,12 +9,16 @@ import java.util.List;
 
 public class GroupDeletionTests extends TestBase {
 
-  @Test
-  public void testGroupDeletion() throws Exception {
+  @BeforeMethod
+  public void ensurePreconditions() {
     app.getNavigationHelper().gotoGroupPage();
     if (!app.getGroupHelper().isThereAGroup()) {
-      app.getGroupHelper().createGroup(new GroupData("Test1", "Test2", "Test3"));
+      app.getGroupHelper().createGroup(new GroupData("Test", "Test2", "Test3"));
     }
+  }
+
+  @Test
+  public void testGroupDeletion() throws Exception {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().deleteSelectedGroups();
