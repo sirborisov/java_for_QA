2d2a985f7fc249e4c33f8de2e240f78d10c788a4
Vladimir Borisov <v.borisov@litebox.ru>
2019-09-26 17:23:48 +0400

    тесты групп. Кеширование результатов проверок и хеширование

--- addressbook-web-tests/src/test/java/ru/qa/borisov/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/qa/borisov/addressbook/tests/GroupCreationTests.java
@@ -10,14 +10,25 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
   @Test
-  public void testGroupCreation() throws Exception {
+  public void testGroupCreation() {
     app.goTo().groupPage();
     Groups before = app.group().all();
     GroupData group = new GroupData().withName("Test5").withHeader("test6").withFooter("test_7");
     app.group().create(group);
+    assertThat(app.group().count(), equalTo(before.size() + 1));
     Groups after = app.group().all();
-    assertThat(after.size(), equalTo(before.size() + 1));
     assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+  }
+
+  @Test
+  public void testBadGroupCreation() {
+    app.goTo().groupPage();
+    Groups before = app.group().all();
+    GroupData group = new GroupData().withName("Test5'").withHeader("test6").withFooter("test_7");
+    app.group().create(group);
+    assertThat(app.group().count(), equalTo(before.size()));
+    Groups after = app.group().all();
+    assertThat(after, equalTo(before));
   }
 
 }
--- addressbook-web-tests/src/test/java/ru/qa/borisov/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/qa/borisov/addressbook/tests/GroupModificationTests.java
@@ -24,8 +24,8 @@ public class GroupModificationTests extends TestBase {
     GroupData modifiedGroup = before.iterator().next();
     GroupData group = new GroupData().withId(modifiedGroup.getId()).withName("Test0").withHeader("test01").withFooter("test02");
     app.group().modify(group);
+    assertThat(app.group().count(), equalTo(before.size()));
     Groups after = app.group().all();
-    assertThat(after.size(), equalTo(before.size()));
     assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
   }
 }
