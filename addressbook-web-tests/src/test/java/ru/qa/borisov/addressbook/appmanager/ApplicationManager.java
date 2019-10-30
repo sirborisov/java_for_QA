83ac528b6a3487ff25774a86e0380172a043defb
Vladimir Borisov <v.borisov@litebox.ru>
2019-10-30 13:26:33 +0400

    тест модификации групп. Сравнение данных, загруженных из БД

--- addressbook-web-tests/src/test/java/ru/qa/borisov/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/qa/borisov/addressbook/appmanager/ApplicationManager.java
@@ -22,6 +22,7 @@ public class ApplicationManager {
   private GroupHelper groupHelper;
   private ContactHelper contactHelper;
   private String browser;
+  private DbHelper dbHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -31,6 +32,8 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+    dbHelper = new DbHelper();
 
     if (browser.equals(BrowserType.CHROME)) {
       wd = new ChromeDriver();
@@ -64,6 +67,10 @@ public class ApplicationManager {
 
   public NavigationHelper goTo() {
     return navigationHelper;
+  }
+
+  public DbHelper db() {
+    return dbHelper;
   }
 
 }
