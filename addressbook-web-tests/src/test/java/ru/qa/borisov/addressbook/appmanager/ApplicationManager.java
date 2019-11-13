e435e9238a4ee808bff60aca6a7ed414532acfa4
Vladimir Borisov <v.borisov@litebox.ru>
2019-11-13 21:31:18 +0400

    Update ApplicationManager.java

--- addressbook-web-tests/src/test/java/ru/qa/borisov/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/qa/borisov/addressbook/appmanager/ApplicationManager.java
@@ -50,7 +50,7 @@ public class ApplicationManager {
     } else {
       DesiredCapabilities capabilities = new DesiredCapabilities();
       capabilities.setBrowserName(browser);
-      capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win7")));
+      capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "WINDOWS")));
       wd= new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
     wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
